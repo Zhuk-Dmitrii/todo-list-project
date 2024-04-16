0880fac2f5424e2c6d1d1277fe7dabc32e3a6679
Zhuk-Dmitrii <fgh298@rambler.ru>
2024-04-16 14:29:49 +0300

    start project, added first task by it-incubator (todo list)

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { TodoList } from './TodoList'
+import './App.css'
+
+export type TTasks = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+export function App() {
+  const task1: Array<TTasks> = [
+    { id: 1, title: 'HTML & CSS', isDone: true },
+    { id: 2, title: 'JS/TS', isDone: true },
+    { id: 1, title: 'ReactJS', isDone: false },
+  ]
+
+  const task2: Array<TTasks> = [
+    { id: 1, title: 'Lineage II', isDone: true },
+    { id: 2, title: 'GTA V', isDone: false },
+    { id: 1, title: 'Dota 2', isDone: true },
+  ]
+
+  return (
+    <div className="app">
+      <TodoList title={'What to learn'} tasks={task1} />
+      <TodoList title={'Games'} tasks={task2} />
+    </div>
+  )
+}
---
+++ src/TodoList.tsx
@@ -0,0 +1,38 @@
+import { TTasks } from './App'
+import './todoList.css'
+
+type TProps = {
+  title: string
+  tasks: Array<TTasks>
+}
+
+export function TodoList(props: TProps) {
+  return (
+    <div className="todo-list__wrapper">
+      <h3 className="todo-list__title">{props.title}</h3>
+      <div className="todo-list__input-container">
+        <input type="text" />
+        <button className="input-container__btn btn">add</button>
+      </div>
+      <ul className="todo-list__items">
+        <li className="todo-list__item">
+          <input type="checkbox" checked={props.tasks[0].isDone} />
+          <span>{props.tasks[0].title}</span>
+        </li>
+        <li className="todo-list__item">
+          <input type="checkbox" checked={props.tasks[1].isDone} />
+          <span>{props.tasks[1].title}</span>
+        </li>
+        <li className="todo-list__item">
+          <input type="checkbox" checked={props.tasks[2].isDone} />
+          <span>{props.tasks[2].title}</span>
+        </li>
+      </ul>
+      <div className="todo-list__btn-controls">
+        <button className="btn-controls__btn btn">All</button>
+        <button className="btn-controls__btn btn">Active</button>
+        <button className="btn-controls__btn btn">Completed</button>
+      </div>
+    </div>
+  )
+}
