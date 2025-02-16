dbb98f5b78aee05c0c69a023f473696ded38ab82
Zhuk-Dmitrii <fgh298@rambler.ru>
2025-02-16 19:23:27 +0300

    feat: added back home button in Not found page

--- src/features/NotFound/NotFound.tsx
+++ src/features/NotFound/NotFound.tsx
@@ -1,11 +1,27 @@
-import { Container, Typography } from '@mui/material'
+import { Button, Container, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+import { PATHS } from '../../app/routers/path'
 
 export function NotFound() {
+  const navigate = useNavigate()
+
+  function handleBackHome() {
+    navigate(PATHS.HOME)
+  }
+
   return (
     <Container
-      sx={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
     >
       <Typography variant="h1" color="error" children={'Not Found'} />
+      <Button children={'вернуться на главную'} onClick={handleBackHome} sx={{ mt: 3 }} />
     </Container>
   )
 }
