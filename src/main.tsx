ce433e69b1d2ca714b1e333636fef508435b4e02
Zhuk-Dmitrii <fgh298@rambler.ru>
2025-02-18 14:17:46 +0300

    feat: delete strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
@@ -8,9 +7,7 @@ import { appRouter } from './app/routers'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={appRouter} />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={appRouter} />
+  </Provider>,
 )
