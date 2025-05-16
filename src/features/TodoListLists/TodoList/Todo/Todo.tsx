371da6a8667ce47506f596c9fbeff5ba649ae889
Zhuk-Dmitrii <fgh298@rambler.ru>
2025-05-16 13:09:13 +0300

    refactor: edited Todo component

--- src/features/TodoListLists/TodoList/Todo/Todo.tsx
+++ src/features/TodoListLists/TodoList/Todo/Todo.tsx
@@ -13,56 +13,55 @@ type TProps = {
   task: TaskType
 }
 
-export const Todo = React.memo((props: TProps) => {
+export const Todo = React.memo(({ todoListId, task }: TProps) => {
   const dispatch = useAppDispatch()
 
   const handleDeleteTask = useCallback(() => {
-    const action = deleteTask({ taskId: props.task.id, todoListId: props.todoListId })
+    const action = deleteTask({ taskId: task.id, todoListId: todoListId })
 
     dispatch(action)
-  }, [props.todoListId, props.task.id, dispatch])
+  }, [todoListId, task.id, dispatch])
 
   const handleChangeCheckbox = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
-      const taskId = props.task.id
       const checked = event.currentTarget.checked
       const status = checked ? TaskStatus.Completed : TaskStatus.New
       const action = updateTask({
-        todoListId: props.todoListId,
-        taskId,
+        todoListId: todoListId,
+        taskId: task.id,
         businessModel: { status },
       })
 
       dispatch(action)
     },
-    [props.task.id, props.todoListId, dispatch],
+    [task.id, todoListId, dispatch],
   )
 
   const handleChangeTaskTitle = useCallback(
     (newValue: string) => {
       const action = updateTask({
-        todoListId: props.todoListId,
-        taskId: props.task.id,
+        todoListId: todoListId,
+        taskId: task.id,
         businessModel: { title: newValue },
       })
 
       dispatch(action)
     },
-    [props.todoListId, props.task.id, dispatch],
+    [todoListId, task.id, dispatch],
   )
 
   return (
-    <ListItem sx={{ p: 0, opacity: props.task.status === TaskStatus.Completed ? 0.5 : 1 }}>
+    <ListItem sx={{ p: 0, opacity: task.status === TaskStatus.Completed ? 0.5 : 1 }}>
       <Checkbox
         onChange={handleChangeCheckbox}
-        checked={props.task.status === TaskStatus.Completed}
+        checked={task.status === TaskStatus.Completed}
         size="small"
         color="success"
         sx={{ mr: 1 }}
       />
       <EditableSpan
         changeValue={handleChangeTaskTitle}
-        title={props.task.title}
+        title={task.title}
         sx={customCSS.editableSpan}
       />
       <IconButton onClick={handleDeleteTask} sx={{ ml: 'auto' }} size="small">
